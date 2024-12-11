c598143f868d4a0936a6096f1170bd15e5c637f4
Akash negi <akashnegi977@gmail.com>
2024-12-11 10:12:41 +0530

    #10516  added ellipsis if more than 5 warnings occurs

--- modules/ui/commit_warnings.js
+++ modules/ui/commit_warnings.js
@@ -19,11 +19,16 @@ export function uiCommitWarnings(context) {
                 issues = issues.filter(function(issue) { return issue.type !== 'help_request'; });
             }
 
+            var displayIssues = issues.slice(0, 5); // Display only the first warning
+            var hiddenIssues = issues.slice(5); // Remaining warnings
+            var hasMoreIssues = issues.length > 5; // Check if there are more than 5 issues
+
+
             var section = severity + '-section';
             var issueItem = severity + '-item';
 
             var container = selection.selectAll('.' + section)
-                .data(issues.length ? [0] : []);
+                .data(displayIssues.length ? [0] : []);
 
             container.exit()
                 .remove();
@@ -45,7 +50,7 @@ export function uiCommitWarnings(context) {
 
 
             var items = container.select('ul').selectAll('li')
-                .data(issues, function(d) { return d.key; });
+                .data(displayIssues, function(d) { return d.key; });
 
             items.exit()
                 .remove();
@@ -92,12 +97,72 @@ export function uiCommitWarnings(context) {
 
             items.selectAll('.issue-message')
                 .text('')
-                .each(function(d) {
+                .each(function(d){
                     return d.message(context)(d3_select(this));
-                });
+                 });
+
+            // Adding ellipsis if there are more issues
+            if (hasMoreIssues) {
+                // IIFE to create a new scope for each instance
+                (function(localContainer) {
+
+                    var ellipsisItem = localContainer.select('ul').selectAll('.ellipsis')
+                        .data([0]);
+
+                    ellipsisItem.enter()
+                        .append('div')
+                        .attr('class', 'ellipsis')
+                        .append('button')
+                        .text('...More warnings')
+                        .on('click', function() {
+                            var hiddenItems = localContainer.select('ul').selectAll('.hidden-warning');
+                            if (!hiddenItems.empty()) {
+                                var isHidden = hiddenItems.style('display') === 'none';
+                                hiddenItems.style('display', isHidden ? 'block' : 'none');
+                                d3_select(this).text(isHidden ? 'Show less' : '...More warnings'); // Change button text
+                            }
+                        });
+
+                    ellipsisItem.exit().remove();
+
+                    var hiddenItems = localContainer.select('ul').selectAll('.hidden-warning')
+                        .data(hiddenIssues, function(d) { return d.key; });
+
+                    var hiddenItemsEnter = hiddenItems.enter()
+                        .append('li')
+                        .attr('class', 'hidden-warning ' + issueItem)
+                        .style('display', 'none'); // Hide the additional warnings initially
+
+                    hiddenItemsEnter.append('button')
+                        .on('mouseover', function(d3_event, d) {
+                            if (d.entityIds) {
+                                context.surface().selectAll(
+                                    utilEntityOrMemberSelector(
+                                        d.entityIds,
+                                        context.graph()
+                                    )
+                                ).classed('hover', true);
+                            }
+                        })
+                        .on('mouseout', function() {
+                            context.surface().selectAll('.hover').classed('hover', false);
+                        })
+                        .on('click', function(d3_event, d) {
+                            context.validator().focusIssue(d);
+                        })
+                        .call(svgIcon('#iD-icon-alert', 'pre-text'))
+                        .append('strong')
+                        .attr('class', 'issue-message')
+                        .text('')
+                        .each(function(d) {
+                            return d.message(context)(d3_select(this));
+                        });
+
+                    hiddenItems.exit().remove();
+                })(container); // Pass the container to the IIFE
+            }
         }
     }
-
 
     return commitWarnings;
 }
