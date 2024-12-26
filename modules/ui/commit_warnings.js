9dd5628b138c61fd6963a7eba9ad11029629455e
Akash negi <akashnegi977@gmail.com>
2024-12-26 21:18:37 +0530

    changed var to let, implemented arrow function where needed, and other requested changes are done

--- modules/ui/commit_warnings.js
+++ modules/ui/commit_warnings.js
@@ -5,95 +5,100 @@ import { uiSection } from '../ui/section';
 import { utilEntityOrMemberSelector } from '../util';
 
 export function uiCommitWarnings(context) {
-    var _issuesBySeverity = {};
+    let _issuesBySeverity = [];
 
     function commitWarnings(selection) {
-        // Wrap the selection in a div with the class modal-section
-        selection = selection.append('div').attr('class', 'modal-section');
-
         // Load issues by severity
         _issuesBySeverity = context.validator()
             .getIssuesBySeverity({ what: 'edited', where: 'all', includeDisabledRules: true });
 
         for (let severity in _issuesBySeverity) {
+
             let issues = _issuesBySeverity[severity];
 
-            if (severity !== 'error') { // exclude 'fixme' and similar - #8603
-                issues = issues.filter(function(issue) { return issue.type !== 'help_request'; });
+            if (severity !== 'error') {
+                // Exclude 'help_request' type issues
+                issues = issues.filter(issue => issue.type !== 'help_request');
             }
 
             if (!issues.length) continue;
 
-            // Create a collapsible section for each severity level
-            var section = uiSection('issues-' + severity, context)
+            const section = uiSection('issues-'+severity , context)
                 .label(() => {
-                    var count = issues.length;
+                    const count = issues.length;
                     return t.append(
                         'inspector.title_count',
                         { title: t('issues.' + severity + 's.list_title'), count: count }
                     );
                 })
-                .disclosureContent(function(selection) {
-                    return renderIssuesList(selection, severity, issues);
-                })
-                .shouldDisplay(function() {
-                    return issues && issues.length;
-                });
+                .disclosureContent(selection => renderIssuesList(selection, issues));
+
+                // Wrap the selection in a div with the class modal-section
+                selection = selection
+                .selectAll('.'+severity + '-section')
+                .data([0]);
+
+                selection = selection.enter()
+                .append('div')
+                .attr('class', 'modal-section')
+                .merge(selection);
 
             // Add the appropriate class for styling based on severity
             selection
                 .call(section.render)
                 .classed(severity + '-section', true);
+
         }
     }
 
-    function renderIssuesList(selection, severity, issues) {
-        selection.selectAll('.issues-list').remove();
-        var container = selection
+
+    function renderIssuesList(selection, issues) {
+
+        let container = selection
+            .selectAll('.changeset-list')
+            .data([0]);
+
+
+        container = container.enter()
             .append('ul')
-            .attr('class', 'changeset-list');
+            .attr('class', 'changeset-list')
+            .merge(container);
 
-        container.exit().remove();
-
-        var items = container.selectAll('li')
-            .data(issues, function(d) { return d.key; });
+        const items = container.selectAll('li')
+            .data(issues, d => d.key);
 
         items.exit().remove();
 
-        var itemsEnter = items.enter()
+        const itemsEnter = items.enter()
             .append('li')
-            .attr('class', function (d) { return 'issue severity-' + d.severity; });
+            .attr('class', d => 'issue severity-' + d.severity);
 
-        var buttons = itemsEnter
+        const buttons = itemsEnter
             .append('button')
-            .on('mouseover', function(d3_event, d) {
+            .on('mouseover', (d3_event, d) => {
                 if (d.entityIds) {
                     context.surface().selectAll(
-                        utilEntityOrMemberSelector(
-                            d.entityIds,
-                            context.graph()
-                        )
+                        utilEntityOrMemberSelector(d.entityIds, context.graph())
                     ).classed('hover', true);
                 }
             })
-            .on('mouseout', function() {
+            .on('mouseout', () => {
                 context.surface().selectAll('.hover').classed('hover', false);
             })
-            .on('click', function(d3_event, d) {
+            .on('click', (d3_event, d) => {
                 context.validator().focusIssue(d);
             });
 
-            var textEnter = buttons
+        const textEnter = buttons
             .append('span')
             .attr('class', 'issue-text');
 
         textEnter
             .append('span')
             .attr('class', 'issue-icon')
-            .each(function(d) {
-                var iconName = '#iD-icon-' + (d.severity === 'warning' ? 'alert' : 'error');
-                d3_select(this)
-                    .call(svgIcon(iconName));
+            .each((d, i, nodes) => {
+                const iconName = '#iD-icon-' + (d.severity === 'warning' ? 'alert' : 'error');
+                d3_select(nodes[i]).call(svgIcon(iconName));
             });
 
         textEnter
@@ -104,9 +109,9 @@ export function uiCommitWarnings(context) {
             .merge(items)
             .selectAll('.issue-message')
             .text('')
-            .each(function(d) {
-                return d.message(context)(d3_select(this));
-            });
+            .each((d, i, nodes) => d.message(context)(d3_select(nodes[i])));
+
+
     }
 
     return commitWarnings;
